223592cbe644e526782efa0bd4b6f648c9f88c86
Isaias Espinosa <isaias.espinosa@gmail.com>
2022-09-30 17:45:56 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -35,15 +36,10 @@ app.use('/api/todos', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
 
-// app.get('/api/usuarios', (req,resp) => {
-//     resp.json({
-//         ok:true,
-//         usuarios:[{
-//             id:123,
-//             nombre: 'Isaias'
-//         }]
-//     })
-// });
+// Lo ultimo
+app.get('*',(req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html'));
+});
 
 
 app.listen(process.env.PORT, () => {
